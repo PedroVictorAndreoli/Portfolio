ed3ae577ea1d0fb861c821d528e647ac39e1900d
PedroVictorAndreoli <114033109+PedroVictorAndreoli@users.noreply.github.com>
2024-08-29 15:39:47 -0300

    mais projetos

---
+++ carousel.js
@@ -0,0 +1,36 @@
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.next');
+const prevButton = document.querySelector('.prev');
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// Arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
+};
+
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+};
+
+// When I click next, move slides to the right
+nextButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling;
+  if (!nextSlide) return; // Stop if there's no next slide
+
+  moveToSlide(track, currentSlide, nextSlide);
+});
+
+// When I click prev, move slides to the left
+prevButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const prevSlide = currentSlide.previousElementSibling;
+  if (!prevSlide) return; // Stop if there's no previous slide
+
+  moveToSlide(track, currentSlide, prevSlide);
+});
